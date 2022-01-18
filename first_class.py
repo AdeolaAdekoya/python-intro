1a65e65f1f93cff7d03c5c8c324bc855de331dd8
Adeola Adekoya <adeolaadekoya11@gmail.com>
2022-01-18 15:52:15 +0100

    variables, values, expressions, operators

---
+++ first_class.py
@@ -0,0 +1,68 @@
+print("welcome to python")
+
+
+# chcking types
+print(type("5"))
+print( type(5))
+print( type(5.0))
+
+
+# changing types
+
+print(type( int ("5")))
+
+# you can chang from intiger, intiger to string
+# string to float to string
+#  intiger - float and intiger
+# float- intiger- float
+# clause- the value has to be a number to change 
+
+
+print(type(9))
+print(type("82"))
+print(type(3.9))
+
+
+print(type(str(9)))
+print(type(int("9")))
+print(type(float(9)))
+
+a = "hii"
+
+print(a)
+
+
+
+easypace = ((8 * 60) + 15) * 2
+tempopace = ((7 * 60) + 12) * 3
+
+my_runtime = easypace + tempopace
+print(my_runtime)
+
+my_answer = my_runtime / 3600
+start = 6 + (52/60)
+print (my_answer + start)
+
+
+
+volume= (4/3)*3.14*(5**3)
+print(volume)
+
+
+price_for_60 = (24.95 * 60 ) * 0.6
+shipping = 3 + (0.75*59)
+
+print(price_for_60+shipping)
+
+area= 20
+cost_of_carpet = 24.99
+cost_of_cushioning_pad = 2.99
+cost_of_labour = 18
+
+
+total_cost = (cost_of_carpet * 20) + (cost_of_cushioning_pad * 20) + (cost_of_labour * 20) + (35)
+
+
+print(total_cost)
+
+
